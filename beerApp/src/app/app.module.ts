24ffcdd02130899e8dd8194eb3490d9089ee62a8
sybille <sybille@pxlwidgets-heroes.com>
2020-07-20 10:57:08 +0200

    Created basic routing

--- beerApp/src/app/app.module.ts
+++ beerApp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BreweryListComponent } from './brewery-list/brewery-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BreweryListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
